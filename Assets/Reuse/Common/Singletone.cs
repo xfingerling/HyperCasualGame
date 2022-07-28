17aef59fb0a092eadb2b5d18f95667c5657a02f6
Oleg Zavhorodnii <oleggenius95@gmail.com>
2022-07-28 12:45:03 +0300

    Update Singletone.cs

--- Assets/Reuse/Common/Singletone.cs
+++ Assets/Reuse/Common/Singletone.cs
@@ -1,18 +1,46 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Singletone : MonoBehaviour
+public class Singletone<T> : MonoBehaviour where T : Component
 {
-    // Start is called before the first frame update
-    void Start()
+    private static T _instance;
+    private static bool _isApplicationQuitting;
+
+    public static T Instance
     {
-        
+        get
+        {
+            if (_isApplicationQuitting)
+                return null;
+
+            if (_instance != null)
+                return _instance;
+
+            _instance = FindObjectOfType<T>();
+            if (_instance != null)
+                return _instance;
+
+            var obj = new GameObject { name = typeof(T).Name };
+            _instance = obj.AddComponent<T>();
+            return _instance;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public virtual void Touch() { }
+
+    protected virtual void Awake()
     {
-        
+        if (_instance == null)
+        {
+            _instance = this as T;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+            Destroy(gameObject);
     }
+
+    protected virtual void OnApplicationQuit()
+    {
+        _isApplicationQuitting = true;
+    }
+}
 }
